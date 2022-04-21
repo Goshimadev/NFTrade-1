f74d56d987a6db5bf71be7a6742135c848c13f9a
levi hicks <lv.hicks@gmail.com>
2022-04-20 22:10:05 -0500

    add data structures

--- contracts/NFTrade.sol
+++ contracts/NFTrade.sol
@@ -2,6 +2,11 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract NFTrade {
-  constructor() public {
+  address[][] public participants; // participants[tradeId] == array of each owner participating in a trade
+  address[][] public tokenContracts; // tokenContracts[tradeId] == array of each NFT involved in a trade
+  uint[][] public tokenIds; // tokenContracts[tradeId] == array of token IDs involved in a trade
+
+  constructor() {
+    
   }
 }
